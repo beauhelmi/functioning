aa341e66e724e3b97c29df464f634707adfc3b03
beauhelmi <beauhelmi@gmail.com>
2024-10-31 17:39:32 +0800

    Functioning selecting

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <title>Document</title>
+</head>
+
+<body>
+
+  <?php
+  $books = [
+    [
+      'name' => 'Do Androids Dream of Electric Sheep',
+      'Author' => 'Philips K. Dick',
+      'releaseYear' => 1968,
+      'purchaseUrl' => 'http://example.com'
+    ],
+    [
+      'name' => 'Project Hail Mary',
+      'Author' => 'Andy Weir',
+      'releaseYear' => 2021,
+      'purchaseUrl' => 'http://example.com'
+    ],
+    [
+      'name' => 'The Martian',
+      'Author' => 'Andy Weir',
+      'releaseYear' => 2011,
+      'purchaseUrl' => 'http://example.com'
+    ],
+  ];
+  ?>
+  <h1>Recommended Books</h1>
+  <ul>
+    <?php foreach ($books as $book) : ?>
+      <?php if ($book['Author'] == 'Andy Weir') : ?>
+        <li>
+          <a href="<?= $book['purchaseUrl'] ?>">
+            <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
+          </a>
+        </li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </ul>
+
+</body>
+
+</html>
